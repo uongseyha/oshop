4aa6dbabc4845d07d3230ecdd86e3abe5bb0e014
Seyha Uong <uong.seyha@gmail.com>
2025-12-21 15:51:33 -0500

    chore: stash/commit before dependency update

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,10 +3,18 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
+import { environment } from '../environments/environment';
+
+import { provideFirebaseApp, initializeApp } from '@angular/fire/app';
+import { provideAuth, getAuth } from '@angular/fire/auth';
+import { provideFirestore, getFirestore } from '@angular/fire/firestore';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
-    provideRouter(routes), provideClientHydration(withEventReplay())
+    provideRouter(routes), provideClientHydration(withEventReplay()),
+    provideFirebaseApp(() => initializeApp(environment.firebase)),
+    provideAuth(() => getAuth()),
+    provideFirestore(() => getFirestore())
   ]
 };
---
+++ src/environments/environment.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyA-pZ5j4O6jUMHuemdP16-j9COrdlVO1Tw',
+    authDomain: 'oshop-seyha.firebaseapp.com',
+    databaseURL: 'https://oshop-seyha.firebaseio.com',
+    projectId: 'oshop-seyha',
+    storageBucket: 'oshop-seyha.firebasestorage.app',
+    messagingSenderId: '323461123259',
+    appId: '1:323461123259:web:d95f6dd7b5f2dafed9ad9e',
+  },
+};
